6b6dfd3e98da8f425892bb9f90f275128cabf0e4
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2024-08-07 09:02:35 +0300

    feat(schema): add customer schema

---
+++ customer/src/customer/schemas/customer.schema.ts
@@ -0,0 +1,15 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type CustomerDocument = Customer & Document;
+
+@Schema()
+export class Customer {
+  @Prop({ type: String, required: true })
+  name: string;
+
+  @Prop({ type: String, required: true, unique: true })
+  email: string;
+}
+
+export const CustomerSchema = SchemaFactory.createForClass(Customer);
